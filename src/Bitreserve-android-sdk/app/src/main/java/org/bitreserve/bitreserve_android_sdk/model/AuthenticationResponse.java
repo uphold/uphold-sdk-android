fb0d2f90b79285089e9826ec72e201b27b3fb409
Sandro Machado <sandro@bitreserve.org>
2015-03-10 13:02:13 +0000

    Add authentication request model
    Added authentication request model class

---
+++ src/Bitreserve-android-sdk/app/src/main/java/org/bitreserve/bitreserve_android_sdk/model/AuthenticationResponse.java
@@ -0,0 +1,56 @@
+package org.bitreserve.bitreserve_android_sdk.model;
+
+/**
+ * This class represents the authentication response model.
+ */
+
+public class AuthenticationResponse {
+
+    private final String access_token;
+    private final String description;
+    private final String expires;
+
+    /**
+     * Constructor.
+     *
+     * @param access_token The access token from the user.
+     * @param description The description from the authentication response.
+     * @param expires The expiration date from the token.
+     */
+
+    public AuthenticationResponse(String access_token, String description, String expires) {
+        this.access_token = access_token;
+        this.description = description;
+        this.expires = expires;
+    }
+
+    /**
+     * Gets the access token.
+     *
+     * @return the access token
+     */
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    /**
+     * Gets the description.
+     *
+     * @return the description
+     */
+
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Gets the expiration date.
+     *
+     * @return the expiration date
+     */
+
+    public String getExpires() {
+        return expires;
+    }
+}

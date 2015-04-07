d50663d7b66b9d0d9ee83d97506ecc32a2289340
Sandro Machado <sandro@bitreserve.org>
2015-04-07 14:42:38 +0100

    Add tests to the transaction model Adding tests to the transaction model.

--- src/Bitreserve-android-sdk/app/src/androidTest/java/org/bitreserve/bitreserve_android_sdk/test/util/Fixtures.java
+++ src/Bitreserve-android-sdk/app/src/androidTest/java/org/bitreserve/bitreserve_android_sdk/test/util/Fixtures.java
@@ -5,6 +5,12 @@ import com.github.javafaker.Faker;
 import org.bitreserve.bitreserve_android_sdk.model.Card;
 import org.bitreserve.bitreserve_android_sdk.model.User;
 import org.bitreserve.bitreserve_android_sdk.model.card.Address;
+import org.bitreserve.bitreserve_android_sdk.model.Transaction;
+import org.bitreserve.bitreserve_android_sdk.model.transaction.Denomination;
+import org.bitreserve.bitreserve_android_sdk.model.transaction.Destination;
+import org.bitreserve.bitreserve_android_sdk.model.transaction.Origin;
+import org.bitreserve.bitreserve_android_sdk.model.transaction.Parameters;
+import org.bitreserve.bitreserve_android_sdk.model.transaction.Source;
 import org.bitreserve.bitreserve_android_sdk.model.user.InternationalizationUserSetting;
 import org.bitreserve.bitreserve_android_sdk.model.user.InternationalizationUserSettings;
 import org.bitreserve.bitreserve_android_sdk.model.user.Settings;
@@ -19,6 +25,120 @@ import java.util.HashMap;
  */
 
 public class Fixtures {
+
+    public static Card loadCard() {
+        return loadCard(null);
+    }
+
+    public static Card loadCard(HashMap<String, String> fields) {
+        final Faker faker = new Faker();
+        final HashMap<String, String> fakerFields = new HashMap<String, String>() {{
+            put("addressKeys", String.format("%s,%s,%s", faker.lorem().fixedString(24), faker.lorem().fixedString(24), faker.lorem().fixedString(24)));
+            put("addressValues", String.format("%s,%s,%s", faker.lorem().fixedString(24), faker.lorem().fixedString(24), faker.lorem().fixedString(24)));
+            put("addressesKeys", String.format("%s,%s,%s", faker.lorem().fixedString(24), faker.lorem().fixedString(24), faker.lorem().fixedString(24)));
+            put("addressesValues", String.format("%s,%s,%s", faker.lorem().fixedString(24), faker.lorem().fixedString(24), faker.lorem().fixedString(24)));
+            put("available", faker.numerify("123456789"));
+            put("balance", faker.numerify("123456789"));
+            put("currency", faker.lorem().fixedString(3));
+            put("id", faker.lorem().fixedString(20));
+            put("label", faker.lorem().fixedString(20));
+            put("lastTransactionAt", faker.lorem().fixedString(24));
+            put("settingsPosition", "1");
+            put("settingsStarred", "true");
+        }};
+
+        if (fields != null) {
+            fakerFields.putAll(fields);
+        }
+
+        HashMap<String, String> address = new HashMap<String, String>() {{
+            ArrayList<String> addressKeys = new ArrayList<>(Arrays.asList(fakerFields.get("addressKeys").split(",")));
+            ArrayList<String> addressValues = new ArrayList<>(Arrays.asList(fakerFields.get("addressValues").split(",")));
+
+            for (int position = 0; position < addressKeys.size(); position++) {
+                put(addressKeys.get(position), addressValues.get(position));
+            }
+        }};
+        ArrayList<Address> addresses = new ArrayList<Address>() {{
+            ArrayList<String> addressesKeys = new ArrayList<>(Arrays.asList(fakerFields.get("addressesKeys").split(",")));
+            ArrayList<String> addressesValues = new ArrayList<>(Arrays.asList(fakerFields.get("addressesValues").split(",")));
+
+            for (int position = 0; position < addressesKeys.size(); position++) {
+                add(new Address(addressesKeys.get(position), addressesValues.get(position)));
+            }
+        }};
+
+        return new Card(fakerFields.get("id"), address, addresses, fakerFields.get("available"), fakerFields.get("balance"), fakerFields.get("currency"), fakerFields.get("label"), fakerFields.get("lastTransactionAt"), new org.bitreserve.bitreserve_android_sdk.model.card.Settings(Integer.parseInt(fakerFields.get("settingsPosition")), Boolean.valueOf(fakerFields.get("settingsStarred"))));
+    }
+
+    public static Transaction loadTransaction() {
+        return loadTransaction(null);
+    }
+
+    public static Transaction loadTransaction(HashMap<String, String> fields) {
+        final Faker faker = new Faker();
+        final HashMap<String, String> fakerFields = new HashMap<String, String>() {{
+            put("denominationAmount", faker.numerify("123456789"));
+            put("denominationCurrency", faker.lorem().fixedString(3));
+            put("denominationPair", faker.lorem().fixedString(6));
+            put("denominationRate", faker.numerify("123456789"));
+            put("destinationAmount", faker.numerify("123456789"));
+            put("destinationBase", faker.numerify("123456789"));
+            put("destinationCardId", faker.lorem().fixedString(24));
+            put("destinationCommission", faker.numerify("123456789"));
+            put("destinationCurrency", faker.lorem().fixedString(3));
+            put("destinationDescription", faker.name().name());
+            put("destinationFee", faker.lorem().fixedString(3));
+            put("destinationRate", faker.lorem().fixedString(3));
+            put("destinationType", faker.lorem().fixedString(6));
+            put("destinationUsername", faker.lorem().fixedString(10));
+            put("originAmount", faker.numerify("123456789"));
+            put("originBase", faker.numerify("123456789"));
+            put("originCardId", faker.lorem().fixedString(24));
+            put("originCommission", faker.numerify("123456789"));
+            put("originCurrency", faker.lorem().fixedString(3));
+            put("originDescription", faker.name().fullName());
+            put("originFee", faker.numerify("123456789"));
+            put("originRate", faker.numerify("123456789"));
+            put("originSourcesAmount", String.format("%s,%s,%s", faker.numerify("123456789"), faker.numerify("123456789"), faker.numerify("123456789")));
+            put("originSourcesId", String.format("%s,%s,%s", faker.lorem().fixedString(24), faker.lorem().fixedString(24), faker.lorem().fixedString(24)));
+            put("originType", faker.lorem().fixedString(10));
+            put("originUsername", faker.name().name());
+            put("parametersCurrency", faker.lorem().fixedString(3));
+            put("parametersMargin", faker.numerify("123456789"));
+            put("parametersPair", faker.lorem().fixedString(6));
+            put("parametersProgress", faker.numerify("123456789"));
+            put("parametersRate", faker.numerify("123456789"));
+            put("parametersTtl", faker.numerify("123456789"));
+            put("parametersTxid", faker.numerify("123456789"));
+            put("parametersType", faker.lorem().fixedString(10));
+            put("transactionCreatedAt", faker.lorem().fixedString(24));
+            put("transactionId", faker.lorem().fixedString(24));
+            put("transactionMessage", faker.lorem().fixedString(24));
+            put("transactionRefundedById", faker.lorem().fixedString(24));
+            put("transactionStatus", faker.lorem().fixedString(24));
+            put("transactionType", faker.lorem().fixedString(24));
+        }};
+
+        if (fields != null) {
+            fakerFields.putAll(fields);
+        }
+
+        Denomination denomination = new Denomination(fakerFields.get("denominationAmount"), fakerFields.get("denominationCurrency"), fakerFields.get("denominationPair"), fakerFields.get("denominationRate"));
+        Destination destination = new Destination(fakerFields.get("destinationCardId"), fakerFields.get("destinationAmount"), fakerFields.get("destinationBase"), fakerFields.get("destinationCommission"), fakerFields.get("destinationCurrency"), fakerFields.get("destinationDescription"), fakerFields.get("destinationFee"), fakerFields.get("destinationRate"), fakerFields.get("destinationType"), fakerFields.get("destinationUsername"));
+        ArrayList<Source> sources = new ArrayList<Source>() {{
+            ArrayList<String> ids = new ArrayList<>(Arrays.asList(fakerFields.get("originSourcesId").split(",")));
+            ArrayList<String> amount = new ArrayList<>(Arrays.asList(fakerFields.get("originSourcesAmount").split(",")));
+
+            for (int counter = 0; counter < ids.size(); counter++) {
+                add(new Source(ids.get(counter), amount.get(counter)));
+            }
+        }};
+        Origin origin = new Origin(fakerFields.get("originCardId"), fakerFields.get("originAmount"), fakerFields.get("originBase"), fakerFields.get("originCommission"), fakerFields.get("originCurrency"), fakerFields.get("originDescription"), fakerFields.get("originFee"), fakerFields.get("originRate"), sources, fakerFields.get("originType"), fakerFields.get("originUsername"));
+        Parameters parameters = new Parameters(fakerFields.get("parametersCurrency"), fakerFields.get("parametersMargin"), fakerFields.get("parametersPair"), fakerFields.get("parametersProgress"), fakerFields.get("parametersRate"), Integer.parseInt(fakerFields.get("parametersTtl")), fakerFields.get("parametersTxid"), fakerFields.get("parametersType"));
+
+        return new Transaction(fakerFields.get("transactionId"), fakerFields.get("transactionCreatedAt"), denomination, destination, fakerFields.get("transactionMessage"), origin, parameters, fakerFields.get("transactionRefundedById"), fakerFields.get("transactionStatus"), fakerFields.get("transactionType"));
+    }
 
     public static User loadUser() {
         return loadUser(null);
@@ -69,51 +189,6 @@ public class Fixtures {
         Status status = new Status(fakerFields.get("emailStatus"), fakerFields.get("identityStatus"), fakerFields.get("overviewStatus"), fakerFields.get("phoneStatus"), fakerFields.get("registrationStatus"), fakerFields.get("reviewStatus"), fakerFields.get("screeningStatus"), fakerFields.get("volumeStatus"));
 
         return new User(fakerFields.get("country"), currencies, fakerFields.get("email"), fakerFields.get("firstName"), fakerFields.get("lastName"), fakerFields.get("name"), settings, fakerFields.get("state"), status, fakerFields.get("username"));
-    }
-
-    public static Card loadCard() {
-        return loadCard(null);
-    }
-
-    public static Card loadCard(HashMap<String, String> fields) {
-        final Faker faker = new Faker();
-        final HashMap<String, String> fakerFields = new HashMap<String, String>() {{
-            put("addressKeys", String.format("%s,%s,%s", faker.lorem().fixedString(24), faker.lorem().fixedString(24), faker.lorem().fixedString(24)));
-            put("addressValues", String.format("%s,%s,%s", faker.lorem().fixedString(24), faker.lorem().fixedString(24), faker.lorem().fixedString(24)));
-            put("addressesKeys", String.format("%s,%s,%s", faker.lorem().fixedString(24), faker.lorem().fixedString(24), faker.lorem().fixedString(24)));
-            put("addressesValues", String.format("%s,%s,%s", faker.lorem().fixedString(24), faker.lorem().fixedString(24), faker.lorem().fixedString(24)));
-            put("available", faker.numerify("123456789"));
-            put("balance", faker.numerify("123456789"));
-            put("currency", faker.lorem().fixedString(3));
-            put("id", faker.lorem().fixedString(20));
-            put("label", faker.lorem().fixedString(20));
-            put("lastTransactionAt", faker.lorem().fixedString(24));
-            put("settingsPosition", "1");
-            put("settingsStarred", "true");
-        }};
-
-        if (fields != null) {
-            fakerFields.putAll(fields);
-        }
-
-        HashMap<String, String> address = new HashMap<String, String>() {{
-            ArrayList<String> addressKeys = new ArrayList<>(Arrays.asList(fakerFields.get("addressKeys").split(",")));
-            ArrayList<String> addressValues = new ArrayList<>(Arrays.asList(fakerFields.get("addressValues").split(",")));
-
-            for (int position = 0; position < addressKeys.size(); position++) {
-                put(addressKeys.get(position), addressValues.get(position));
-            }
-        }};
-        ArrayList<Address> addresses = new ArrayList<Address>() {{
-            ArrayList<String> addressesKeys = new ArrayList<>(Arrays.asList(fakerFields.get("addressesKeys").split(",")));
-            ArrayList<String> addressesValues = new ArrayList<>(Arrays.asList(fakerFields.get("addressesValues").split(",")));
-
-            for (int position = 0; position < addressesKeys.size(); position++) {
-                add(new Address(addressesKeys.get(position), addressesValues.get(position)));
-            }
-        }};
-
-        return new Card(fakerFields.get("id"), address, addresses, fakerFields.get("available"), fakerFields.get("balance"), fakerFields.get("currency"), fakerFields.get("label"), fakerFields.get("lastTransactionAt"), new org.bitreserve.bitreserve_android_sdk.model.card.Settings(Integer.parseInt(fakerFields.get("settingsPosition")), Boolean.valueOf(fakerFields.get("settingsStarred"))));
     }
 
 }

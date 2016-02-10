80af5563cb259eec12628930344f7b669e666065
Sandro Machado <sandro@uphold.com>
2016-02-10 14:27:00 +0000

    Add the version from the build gradle file

--- src/app/src/main/java/com/uphold/uphold_android_sdk/config/GlobalConfigurations.java
+++ src/app/src/main/java/com/uphold/uphold_android_sdk/config/GlobalConfigurations.java
@@ -6,7 +6,6 @@ package com.uphold.uphold_android_sdk.config;
 
 public class GlobalConfigurations {
 
-    public static final String UPHOLD_SDK_VERSION = "0.0.7";
     public static final String SDK_GITHUB_URL = "https://github.com/uphold/uphold-sdk-android";
 
 }
